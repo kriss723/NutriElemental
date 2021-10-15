fc2d8f0c685748d9b5a7d16249740f29e5c65a60
jarleysaldarriaga <jesaldarriaga33@gmail.com>
2021-10-14 21:05:20 -0500

    implementacion del delete coach y nutricionista

--- NutriElemental.App/NutriElemental.App.Presentacion/Pages/Coachs/Eliminar.cshtml.cs
+++ NutriElemental.App/NutriElemental.App.Presentacion/Pages/Coachs/Eliminar.cshtml.cs
@@ -4,13 +4,36 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using NutriElemental.App.Persistencia;
+using NutriElemental.App.Dominio;
 
 namespace NutriElemental.App.Presentacion.Pages.Coachs
 {
     public class EliminarModel : PageModel
     {
-        public void OnGet()
+        private readonly IRepositorioCoach _repoCoach;
+        public Coach Coach{get;set;}
+        public EliminarModel(IRepositorioCoach _repoCoach)
         {
+            this._repoCoach = _repoCoach;
+        }
+        public IActionResult OnGet(int id)
+        {
+            Coach = _repoCoach.GetCoach(id);
+            if(Coach == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Page();
+            }
+        }
+
+        public IActionResult OnPost(int id)
+        {
+            _repoCoach.DeleteCoach(id);
+            return RedirectToPage("Index");
         }
     }
 }
--- NutriElemental.App/NutriElemental.App.Presentacion/Pages/Nutricionistas/Eliminar.cshtml.cs
+++ NutriElemental.App/NutriElemental.App.Presentacion/Pages/Nutricionistas/Eliminar.cshtml.cs
@@ -4,13 +4,36 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using NutriElemental.App.Persistencia;
+using NutriElemental.App.Dominio;
 
 namespace NutriElemental.App.Presentacion.Pages.Nutricionistas
 {
     public class EliminarModel : PageModel
     {
-        public void OnGet()
+        private readonly IRepositorioNutricionista _repoNutricionista;
+        public Nutricionista Nutricionista{set;get;}
+        public EliminarModel(IRepositorioNutricionista _repoNutricionista)
         {
+            this._repoNutricionista = _repoNutricionista;
+        }
+        public IActionResult OnGet(int id)
+        {
+            Nutricionista = _repoNutricionista.GetNutricionista(id);
+            if(Nutricionista == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Page();
+            }
+        }
+
+        public IActionResult OnPost(int id)
+        {
+            _repoNutricionista.DeleteNutricionista(id);
+            return RedirectToPage("Index");
         }
     }
 }
